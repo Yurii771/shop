76bad35c9e3bc88c21a1e550552c1d4cbada9a6b
Aleksandr <map.m@ua.fm>
2016-04-17 08:43:22 +0300

    ADD EdpModuleLayouts

--- module/Admin/src/Admin/Controller/AuthController.php
+++ module/Admin/src/Admin/Controller/AuthController.php
@@ -5,7 +5,7 @@ namespace Admin\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
+class AuthController extends AbstractActionController
 {
     public function indexAction()
     {
---
+++ module/EdpModuleLayouts/Module.php
@@ -0,0 +1,31 @@
+<?php
+namespace EdpModuleLayouts;
+
+class Module
+{
+    public function onBootstrap($e)
+    {
+        $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
+            $controller      = $e->getTarget();
+            $controllerClass = get_class($controller);
+            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+            $config          = $e->getApplication()->getServiceManager()->get('config');
+
+            $routeMatch = $e->getRouteMatch();
+            $actionName = strtolower($routeMatch->getParam('action', 'not-found')); // get the action name
+            $controllerName = $routeMatch->getParam('controller', 'not-found'); // get the controller name
+            
+            if (isset($config['module_layouts'][$moduleNamespace][$controllerName])) {
+                if (isset($config['module_layouts'][$moduleNamespace][$controllerName][$actionName])) {
+                    $controller->layout($config['module_layouts'][$moduleNamespace][$controllerName][$actionName]);
+                }elseif(isset($config['module_layouts'][$moduleNamespace][$controllerName]['default'])) {
+                    $controller->layout($config['module_layouts'][$moduleNamespace][$controllerName]['default']);
+                }
+            }elseif(isset($config['module_layouts'][$moduleNamespace]['default'])){
+                $controller->layout($config['module_layouts'][$moduleNamespace]['default']);
+            }
+
+
+        }, 100);
+    }
+}
