8e7650c65ebcad14968cc30dc25d8f695fa22656
TonyShul <shulga.anton.vasilevich@gmail.com>
2016-04-18 23:42:10 +0300

    OrderController просмотр заказов дубль_2

--- module/Admin/src/Admin/Controller/OrderController.php
+++ module/Admin/src/Admin/Controller/OrderController.php
@@ -5,10 +5,24 @@ namespace Admin\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
+
+class OrderController extends AbstractActionController
 {
     public function indexAction()
     {
+        $entityManager =$this->getServiceLocator()->get('Doctrine\ORM\EntityManager') ;
+        $query=$entityManager->createQuery('SELECT u FROM Admin\Entity\Orders u ORDER BY u.id DESC');
+        $rows=$query->getResult();
+
+        return array('orders'=> $rows);
+        
+    }
+    
+        public function editAction()
+    {
         return new ViewModel();
     }
+    
+
+    
 }
