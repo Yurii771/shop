4f89e367215c4bcb3306d7d93b1398a5816a387c
TonyShul <shulga.anton.vasilevich@gmail.com>
2016-04-19 22:02:59 +0300

    OrderController обновленный indexAction и ndex.phtml

--- module/Admin/src/Admin/Controller/OrderController.php
+++ module/Admin/src/Admin/Controller/OrderController.php
@@ -11,9 +11,10 @@ class OrderController extends AbstractActionController
     public function indexAction()
     {
         $entityManager =$this->getServiceLocator()->get('Doctrine\ORM\EntityManager') ;
-        $query=$entityManager->createQuery('SELECT u FROM Admin\Entity\Orders u ORDER BY u.id DESC');
+        //$query=$entityManager->createQuery('SELECT u FROM Admin\Entity\Orders u ORDER BY u.id DESC');
+        $query=$entityManager->createQuery('SELECT u,a.orderStatus FROM Admin\Entity\Orders u JOIN Admin\Entity\OrderStatus a WHERE u.orderStatus=a.id   ORDER BY u.id DESC');
         $rows=$query->getResult();
-
+        //var_dump($rows); die();
         return array('orders'=> $rows);
         
     }
