a19f63d7a7380c10058b977840ce9f4d977fb2ce
AleksandrMishchaniuk <map.m@ua.fm>
2016-04-29 23:30:59 +0300

    Fix Guest/config

--- module/Guest/config/module.config.php
+++ module/Guest/config/module.config.php
@@ -32,55 +32,6 @@ return array(
         ),
         'factories' => array(
             'Guest\Controller\Category' => 'Guest\Factory\CategoryControllerFactory',
-        ),
-    ),
-    'view_manager' => array(
-        'template_path_stack' => array(
-            'Guest' => __DIR__ . '/../view',
-        ),
-        'template_map' => array(
-            'layout/guest'  => __DIR__ . '/../view/layout/guest.phtml',
-            
-        ),
-    ),
-    
-    'module_layouts' => array(
-        'Guest' => array(
-            'default' => 'layout/guest',
-        ),
-     ),
-);
-<?php
-return array(
-    'router' => array(
-        'routes' => array(
-            'guest' => array(
-                'type'    => 'Segment',
-                'options' => array(
-                    'route'    => '/[:controller[/:action[/:id]]]',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Guest\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                    'constraints' => array(
-                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ),
-                ),
-            
-            ),
-        ),
-    ),
-
-    'controllers' => array(
-        'invokables' => array(
-            'Guest\Controller\Index' => 'Guest\Controller\IndexController',
-            'Guest\Controller\Payment' => 'Guest\Controller\PaymentController',
-        ),
-        'factories' => array(
-            'Guest\Controller\Category' => 'Guest\Factory\CategoryControllerFactory',
             'Guest\Controller\Order' => 'Guest\Factory\OrderControllerFactory',
         ),
     ),
@@ -90,6 +41,7 @@ return array(
         ),
         'template_map' => array(
             'layout/guest'  => __DIR__ . '/../view/layout/guest.phtml',
+            
         ),
     ),
     
