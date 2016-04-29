7a6b04a20b38b81483e746a24e6278cb1ce4f368
alexchep <alexchep92@gmail.com>
2016-04-30 00:41:28 +0300

    mainpage + contacts (merge with Yura)

---
+++ module/Guest/config/module.config.php
@@ -0,0 +1,52 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'guest' => array(
+                'type'    => 'Segment',
+                'options' => array(
+                    'route'    => '/[:controller[/:action[/:id]]]',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Guest\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                    'constraints' => array(
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                ),
+            
+            ),
+        ),
+    ),
+
+    'controllers' => array(
+        'invokables' => array(
+            'Guest\Controller\Index' => 'Guest\Controller\IndexController',
+            'Guest\Controller\About' => 'Guest\Controller\AboutController',
+            'Guest\Controller\Contacts' => 'Guest\Controller\ContactsController',
+            'Guest\Controller\Payment' => 'Guest\Controller\PaymentController',
+            'Guest\Controller\Goods' => 'Guest\Controller\GoodsController',
+        ),
+        'factories' => array(
+            'Guest\Controller\Category' => 'Guest\Factory\CategoryControllerFactory',
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Guest' => __DIR__ . '/../view',
+        ),
+        'template_map' => array(
+            'layout/guest'  => __DIR__ . '/../view/layout/guest.phtml',
+            
+        ),
+    ),
+    
+    'module_layouts' => array(
+        'Guest' => array(
+            'default' => 'layout/guest',
+        ),
+     ),
+);
