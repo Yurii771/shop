e9d81455bac261a991c3ebd3f9dc31f92e8f42be
NataliaMarch <nl89@bk.ru>
2016-04-30 09:30:30 +0300

    Доделана вьюха на категории, нужно еще стили поправить

--- module/Guest/config/module.config.php
+++ module/Guest/config/module.config.php
@@ -26,6 +26,7 @@ return array(
         'invokables' => array(
             'Guest\Controller\Index' => 'Guest\Controller\IndexController',
             'Guest\Controller\Payment' => 'Guest\Controller\PaymentController',
+            'Guest\Controller\Goods' => 'Guest\Controller\GoodsController',
         ),
         'factories' => array(
             'Guest\Controller\Category' => 'Guest\Factory\CategoryControllerFactory',
@@ -38,55 +39,6 @@ return array(
         ),
         'template_map' => array(
             'layout/guest'  => __DIR__ . '/../view/layout/guest.phtml',
-        ),
-    ),
-    
-    'module_layouts' => array(
-        'Guest' => array(
-            'default' => 'layout/guest',
-        ),
-     ),
-);
-<?php
-return array(
-    'router' => array(
-        'routes' => array(
-            'guest' => array(
-                'type'    => 'Segment',
-                'options' => array(
-                    'route'    => '/[:controller[/:action[/:id]]]',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Guest\Controller',
-                        'controller'    => 'Index',
-                        'action'        => 'index',
-                    ),
-                    'constraints' => array(
-                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                        'id'     => '[0-9]+',
-                    ),
-                ),
-            
-            ),
-        ),
-    ),
-
-    'controllers' => array(
-        'invokables' => array(
-            'Guest\Controller\Index' => 'Guest\Controller\IndexController',
-            'Guest\Controller\Goods' => 'Guest\Controller\GoodsController',
-        ),
-        'factories' => array(
-            'Guest\Controller\Category' => 'Guest\Factory\CategoryControllerFactory',
-        ),
-    ),
-    'view_manager' => array(
-        'template_path_stack' => array(
-            'Guest' => __DIR__ . '/../view',
-        ),
-        'template_map' => array(
-            'layout/guest'  => __DIR__ . '/../view/layout/guest.phtml',
-            
         ),
     ),
     
