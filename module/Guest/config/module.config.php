7a19e78caa68b48fedeeadd209750fedcc48e983
alexchep <alexchep92@gmail.com>
2016-05-03 18:29:08 +0300

    Nikolay task

---
+++ module/Guest/config/module.config.php
@@ -0,0 +1,46 @@
+<?php
+return array(
+    'router' => array(
+        'routes' => array(
+            'guest' => array(
+                'type'    => 'Segment',
+                'options' => array(
+                    'route'    => '/[:controller[/:action[/:id]]]',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Guest\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                    'constraints' => array(
+                        'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Guest\Controller\Index' => 'Guest\Controller\IndexController',
+        ),
+        'factories' => array(
+            'Guest\Controller\Cart' => 'Guest\Factory\CartControllerFactory',
+			'Guest\Controller\Category' => 'Guest\Factory\CategoryControllerFactory',
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'Guest' => __DIR__ . '/../view',
+        ),
+        'template_map' => array(
+            'layout/guest'  => __DIR__ . '/../view/layout/guest.phtml',
+        ),
+    ),
+    
+    'module_layouts' => array(
+        'Guest' => array(
+            'default' => 'layout/guest',
+        ),
+     ),
+);
---
+++ module/Guest/src/Guest/Controller/CartController.php
@@ -0,0 +1,71 @@
+<?php
+namespace Guest\Controller;
+
+use Application\Controller\BaseAdminController as BaseController;
+use Zend\View\Model\ViewModel;
+use Zend\Validator\Regex;
+use Admin\Entity\Goods;
+
+class CartController extends BaseController
+{
+    public function __construct($em) {
+        $this->_entityManager = $em;
+    }
+    
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    public function addAction()
+    {
+        if(!isset($_SESSION['orders'])){
+            $_SESSION['orders']=array();
+        }
+        $url=$_SERVER['REQUEST_URI'];
+        $url=explode('/', $url);
+        $id=$url[count($url)-1];
+        $id=1;
+        $count=$_POST['count'];
+        $query=$this->getEntityManager()->createQuery('SELECT u FROM Admin\Entity\Goods u where u.id='.$id);
+        $good_data=$query->getResult();
+        $arr['id']=$good_data[0]->getId();
+        $arr['name']=$good_data[0]->getName();
+        $arr['shortDescription']=$good_data[0]->getShortDescription();
+        $arr['count']=$count;
+        $arr['max_count']=$good_data[0]->getCount();
+        $arr['cost']=$good_data[0]->getCost();
+        $arr['photo']=$good_data[0]->getPhoto();
+        array_push($_SESSION['orders'], $arr);
+        return $this->redirect()->toRoute('guest', array('controller' => 'cart', 'action' => 'index'));
+    }
+    
+    public function editAction()
+    {
+        $url=$_SERVER['REQUEST_URI'];
+        $id=explode('/', $url);
+        $id=$id[count($id)-1];
+        foreach($_SESSION['orders'] as $key=>$ses){
+            if($ses['id']==$id){
+                if($_POST['count']<=$_SESSION['orders'][$key]['max_count']){
+                    $_SESSION['orders'][$key]['count']=$_POST['count'];
+                }
+            }
+        }
+        return $this->redirect()->toRoute('guest', array('controller' => 'cart', 'action' => 'index'));
+    }
+    
+    public function deleteAction()
+    {
+        $url=$_SERVER['REQUEST_URI'];
+        $id=explode('/', $url);
+        $id=$id[count($id)-1];
+        foreach($_SESSION['orders'] as $key=>$ses){
+            if($ses['id']==$id){
+                unset($_SESSION['orders'][$key]);
+            }
+        }
+        //session_unset();
+        return $this->redirect()->toRoute('guest', array('controller' => 'cart', 'action' => 'index'));
+    }
+}
