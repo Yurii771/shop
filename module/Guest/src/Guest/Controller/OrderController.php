5a42032d154355b506f2887c18fbff497a014f83
AleksandrMishchaniuk <map.m@ua.fm>
2016-05-15 19:52:58 +0300

    Adjusted Cart and Order

--- module/Guest/src/Guest/Controller/OrderController.php
+++ module/Guest/src/Guest/Controller/OrderController.php
@@ -20,35 +20,38 @@ class OrderController extends BaseController {
         $payment = $payment_query->getResult();
         $delivery_query = $em->createQuery('SELECT u.id, u.deliveryType FROM Admin\Entity\Delivery u ');
         $delivery = $delivery_query->getResult();
-        return array('cities' => $cities, 'payment' => $payment, 'delivery' => $delivery);
+        if (isset($_SESSION['orders'])) {
+            $cart = $_SESSION['orders'];
+        }
+        return array('cities' => $cities, 'payment' => $payment, 'delivery' => $delivery, 'cart' => $cart);
     }
 
     public function confirmAction() {
         $data = $this->request->getPost();
         if (isset($data['customerName']) && isset($data['customerSurname']) && isset($data['adress']) && isset($data['customerPhone']) && isset($data['customerEmail'])) {
-            $validator = new Regex(array('pattern' => '/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/'));
+            $validator = new Regex(array('pattern' => '/\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}/'));
             if ($validator->isValid($data['customerPhone'])) {
                 $em = $this->getEntityManager();
                 $status = 'success';
-                $message = 'Заказ успешно выполнен';
-//                if (isset($_SESSION['orders'])) {
-//                    $goodsInOrder = array();
-//                    for ($i = 0; $i < count($_SESSION['orders']); $i++) {
-//                        $id = $_SESSION['orders'][$i]['id'];
-//                        $count = $_SESSION['orders'][$i]['count'];
-//                        array_push($goodsInOrder, $id => $count);
-//                    }
-//                    $goodsInOrder = json_encode($goodsInOrder);
-//                }
+                $message = 'Ваш заказ оформлен';
+                $goodsInOrder = array();
+                if (isset($_SESSION['orders'])) {
+                    for ($i = 0; $i < count($_SESSION['orders']); $i++) {
+                        $id = $_SESSION['orders'][$i]['goods']->getId();
+                        $count = $_SESSION['orders'][$i]['count'];
+                        $goodsInOrder[$id] = $count;
+                    }
+                    unset($_SESSION['orders']);
+                }
                 $order = new Orders;
                 $payment = $em->find('Admin\Entity\Payment', $data['payment']);
                 $delivery = $em->find('Admin\Entity\Delivery', $data['delivery']);
                 $city = $em->find('Admin\Entity\Cities', $data['city']);
-                $orderStatus = $em->find('Admin\Entity\OrderStatus', 1);
+                $orderStatus = $em->find('Admin\Entity\OrderStatus', 2);
                 $data['payment'] = $payment;
                 $data['delivery'] = $delivery;
                 $data['city'] = $city;
-                $data['orderList'] = 'Тестовый вариант. Для получения заказа раскомментируй строки 35-45, 56 файла ордерконтроллер.';
+                $data['orderList'] = json_encode($goodsInOrder);
                 $data['orderStatus'] = $orderStatus;
 //                $order->setOrder($goodsInOrder);        
                 $order->exchangeArray($data);
